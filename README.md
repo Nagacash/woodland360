315127a56ae913d3b400efe8ffab7cd6c928f549
Moe <chosenfewrecords@hotmail.de>
2025-07-13 13:18:48 +0200

    feat: add autoplaying music and cookie banner

---
+++ README.md
@@ -0,0 +1,6 @@
+# [gsap] ❍ Horizontal Scroll Layout Explorations with GSAP and ScrollTrigger
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/filipz/pen/qEBoPWz](https://codepen.io/filipz/pen/qEBoPWz).
+
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: 'public' },
+      ],
+    }),
+  ],
+};
